35e2cce98bfa5d3c0da6420f8e521f46433b3301
Bill Randall <william.randall@cru.org>
2018-03-12 16:53:07 -0400

    Create a db model for api keys

---
+++ models/db/api-key.model.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const {Context} = require('../context')
+
+module.exports = (sequelize, DataTypes) => {
+  const Score = sequelize.define('Api_Key', {
+    system: DataTypes.STRING,
+    api_pattern: DataTypes.ARRAY(DataTypes.STRING),
+    api_key: DataTypes.UUID,
+    contact_email: DataTypes.STRING
+  })
+}
