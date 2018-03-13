4bb882307f6c915fb74da0b6d2cb8b90f319dcf4
Bill Randall <william.randall@cru.org>
2018-03-13 09:13:03 -0400

    Add db migration for api keys

---
+++ db/migrate/20180313090900-create-api-key.js
@@ -0,0 +1,26 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('api_keys', {
+      api_key: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      system: {
+        type: Sequelize.STRING,
+        defaultValue: "unknown"
+      },
+      api_pattern: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        defaultValue: []
+      },
+      contact_email: {
+        type: Sequelize.STRING
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('api_keys')
+  }
+}
