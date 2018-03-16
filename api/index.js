b15c41842700ec770cd98bb913da4685b4a16390
Bill Randall <william.randall@cru.org>
2018-03-16 15:28:28 -0400

    Add an error handler for 401 errors

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const swaggerizeExpress = require('swaggerize-express')
 const api = express()
 const rollbar = require('../config/rollbar')
 const bodyParser = require('body-parser')
+const unauthorizedErrorHandler = require(path.join(__dirname, 'errors/unauthorized'))
 
 api.use(bodyParser.json())
 api.use(swaggerizeExpress({
@@ -14,5 +15,6 @@ api.use(swaggerizeExpress({
   docspath: '/api-docs',
   handlers: path.join(__dirname, 'controllers')
 }))
+api.use(unauthorizedErrorHandler)
 
 module.exports.handler = rollbar.lambdaHandler(serverless(api))
