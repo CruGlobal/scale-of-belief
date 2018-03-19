3fab91b0f07fe6424f899120edc24162bd0ac82e
Bill Randall <william.randall@cru.org>
2018-03-19 14:32:07 -0400

    Get score from the database or 404

--- api/controllers/score.js
+++ api/controllers/score.js
@@ -1,18 +1,24 @@
 'use strict'
 
+const Score = require('../../models/score')
+
 module.exports = {
   get: function (request, response) {
-    response.json(
-      {
-        uri: 'string',
-        score: {
-          unaware: 0,
-          curious: 0,
-          follower: 0,
-          guide: 0,
-          confidence: 0
-        }
-      })
+    var uri = request.query['uri'].toLowerCase()
+    Score.findOne({
+      where: {
+        uri: uri
+      }
+    }).then((score) => {
+      if (score) {
+        response.json(score)
+      } else {
+        response.status(404)
+        response.json({
+          message: 'Not Found'
+        })
+      }
+    })
   },
 
   post: function (request, response) {
