91c5ed23edf8b39df35ada80a5296c2905627977
Bill Randall <william.randall@cru.org>
2018-03-19 16:11:38 -0400

    Get multiple scores from the db

--- api/controllers/scores.js
+++ api/controllers/scores.js
@@ -1,20 +1,26 @@
 'use strict'
 
+const Score = require('../../models/score')
+const {Op} = require('sequelize')
+
 module.exports = {
   get: function (request, response) {
-    response.json(
-      [
-        {
-          uri: 'string',
-          score: {
-            unaware: 0,
-            curious: 0,
-            follower: 0,
-            guide: 0,
-            confidence: 0
-          }
+    var uriPrefix = request.query['uri'].toLowerCase()
+    Score.findAll({
+      where: {
+        uri: {
+          [Op.like]: uriPrefix + '%'
         }
-      ]
-    )
+      }
+    }).then((scores) => {
+      if (scores && scores.length > 0) {
+        response.json(scores)
+      } else {
+        response.status(404)
+        response.json({
+          message: 'Not Found'
+        })
+      }
+    })
   }
 }
