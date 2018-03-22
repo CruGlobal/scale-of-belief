11a9bda602a70d471e4056c475353b77c4fdbfb3
Brian Zoetewey <brian.zoetewey@ccci.org>
2018-03-22 10:45:57 -0400

    Wipe database (truncate all models) before test.

--- test/global-teardown.js
+++ test/global-teardown.js
@@ -1,13 +1,8 @@
 'use strict'
 
 const sequelize = require('../config/sequelize')
-const {forEach} = require('lodash')
 
 module.exports = () => {
-  // Destroy all Model instances (wipe the database) then close database connections
-  const destroyed = []
-  forEach(sequelize.models || [], (value) => {
-    destroyed.push(value.destroy({truncate: true}))
-  })
-  return Promise.all(destroyed).then(() => sequelize.close())
+  // close database connections
+  return sequelize.close()
 }
