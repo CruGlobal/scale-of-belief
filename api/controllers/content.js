a8a86e48b24f1695d0ef538d55900ef86cd92509
Bill Randall <william.randall@cru.org>
2018-03-22 16:26:11 -0400

    Pull the content uris without scores from the db

--- api/controllers/content.js
+++ api/controllers/content.js
@@ -1,7 +1,26 @@
 'use strict'
 
-module.exports = {
-  get: function (request, response) {
-    response.json([ 'unknown' ])
-  }
+const sequelize = require('../../config/sequelize')
+const {forEach} = require('lodash')
+
+const get = (request, response) => {
+  var uri = removeQueryParameters(request.query['uri'].toLowerCase())
+  sequelize.query(
+    'SELECT events.uri FROM events LEFT JOIN scores USING (uri) WHERE scores.uri IS NULL AND events.uri LIKE(\'' + uri + '%\')',
+    {
+      type: sequelize.QueryTypes.SELECT
+    }
+  ).then((results) => {
+    var uris = []
+    forEach(results, (result) => {
+      uris.push(result.uri)
+    })
+    response.json(uris)
+  })
 }
+
+const removeQueryParameters = (uri) => {
+  return uri.split('?')[0].split('#')[0]
+}
+
+module.exports = { get: get }
