20082aaf2463fefb92eb36f5379e6da06d379bb3
Brian Zoetewey <brian.zoetewey@ccci.org>
2018-03-28 14:46:58 -0400

    Fix missed usages of sequelize.

--- api/controllers/content.js
+++ api/controllers/content.js
@@ -5,7 +5,7 @@ const {forEach} = require('lodash')
 
 const get = (request, response) => {
   var uri = removeQueryParameters(request.query['uri'].toLowerCase())
-  sequelize.query(
+  sequelize().query(
     'SELECT events.uri FROM events LEFT JOIN scores USING (uri) WHERE scores.uri IS NULL AND events.uri LIKE(:uri)',
     {
       replacements: { uri: uri + '%' },
