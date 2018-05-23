0922f3f24b1c8000cf7ba0890210a288b0726975
Adam Meyer <adam.meyer@cru.org>
2018-05-23 13:39:41 -0400

    lookup user's guid via key api before adding

--- api/controllers/api-user.js
+++ api/controllers/api-user.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const ApiUser = require('../../models/api-user')
+const https = require('https')
+const xml2js = require('xml2js')
 
 const get = (request, response) => {
   const guid = request.query['guid']
@@ -17,10 +19,43 @@ const get = (request, response) => {
 }
 
 const post = (request, response) => {
-  const requestBody = request.body
-  ApiUser.save(requestBody).then(() => {
-    response.json(requestBody)
-  })
+  let requestBody = request.body
+
+  // lookup guid
+  if (!requestBody.guid && requestBody.contact_email) {
+    const req = https.get('https://thekey.me/cas/api/' + process.env.THE_KEY_API_KEY + '/user/attributes?email=' + requestBody.contact_email, (res) => {
+
+      if (res.statusCode !== 200) {
+        response.status(res.statusCode)
+        response.end(res.statusCode === 404 ? 'The email used is not an existing Key user account.' : 'Guid lookup error')
+        return
+      }
+
+      res.on('data', (d) => {
+        // parse response xml to get guid
+        xml2js.parseString(d.toString(), (err, result) => {
+          if (!err && result && result['attributes'] && result['attributes']['attribute']) {
+            const attributes = result['attributes']['attribute']
+            attributes.forEach((attr) => {
+              if (attr['$']['name'] === 'ssoGuid') {
+                requestBody.guid = attr['$']['value'].toLowerCase()
+              }
+            })
+          }
+
+          ApiUser.save(requestBody).then(() => {
+            response.json(requestBody)
+          })
+        })
+      })
+    })
+
+    req.end()
+  } else {
+    ApiUser.save(requestBody).then(() => {
+      response.json(requestBody)
+    })
+  }
 }
 
 module.exports = {
