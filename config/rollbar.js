91969bd116b5d717c9c713b009e9edc4cf46897d
Brian Zoetewey <brian.zoetewey@ccci.org>
2018-05-24 16:02:57 -0400

    Proxy rollbar messages to logger before sending to rollbar.

--- config/rollbar.js
+++ config/rollbar.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const Rollbar = require('rollbar')
+const logger = require('./logger')
 const {includes} = require('lodash')
+const proxyMap = {critical: 'crit', error: 'error', warning: 'warning', info: 'info', debug: 'debug'}
 
 const rollbar = new Rollbar({
   // https://rollbar.com/docs/notifier/rollbar.js/#configuration-reference
@@ -13,4 +15,16 @@ const rollbar = new Rollbar({
   }
 })
 
-module.exports = rollbar
+// Proxy specific methods to logger before sending to rollbar
+module.exports = new Proxy(rollbar, {
+  get: function (obj, prop) {
+    if (prop in proxyMap) {
+      const origFunc = obj[prop]
+      return function (...args) {
+        logger[proxyMap[prop]].apply(this, args)
+        return origFunc.apply(this, args)
+      }
+    }
+    return obj[prop]
+  }
+})
