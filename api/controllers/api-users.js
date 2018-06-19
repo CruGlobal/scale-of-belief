30640e6753195d7fea9b2a026c4ea9fd8da59e7b
Bill Randall <william.randall@cru.org>
2018-06-19 14:58:22 -0400

    Enable pagination on the api-users endpoint

--- api/controllers/api-users.js
+++ api/controllers/api-users.js
@@ -1,11 +1,36 @@
 'use strict'
 
 const ApiUser = require('../../models/api-user')
+const util = require('../util/util')
 
 module.exports = {
   get: (request, response) => {
-    ApiUser.findAll().then((users) => {
-      response.json(users)
+    let page = request.query['page']
+    let perPage = request.query['per_page']
+    let orderBy = request.query['order_by']
+    let order = request.query['order']
+
+    if (!util.isInt(page) || !util.isInt(perPage)) {
+      page = 1
+      perPage = 25
+    }
+
+    if (!orderBy) {
+      orderBy = 'guid'
+    }
+
+    if (!order) {
+      order = 'ASC'
+    }
+
+    const offset = (page - 1) * perPage
+
+    ApiUser.findAndCountAll({
+      limit: perPage,
+      offset: offset,
+      order: [[orderBy, order]]
+    }).then((users) => {
+      response.json({ data: users.rows, meta: { total: users.count }})
     })
   }
 }
