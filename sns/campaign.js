1d609ed5226505f04c88c6da59f7d86d7e4c0d0b
Bill Randall <william.randall@cru.org>
2018-09-11 08:10:17 -0400

    Extract functions to be testable individually

--- sns/campaign.js
+++ sns/campaign.js
@@ -5,87 +5,87 @@ const AdobeCampaign = require('../config/adobe-campaign')
 const {forEach} = require('lodash')
 const ACCESS_TOKEN = 'scale-of-belief-lambda-campaign-access-token'
 
-module.exports.handler = rollbar.lambdaHandler((lambdaEvent, lambdaContext, lambdaCallback) => {
-  let message = lambdaEvent.Records[0].Sns.Message
-  message = JSON.parse(message)
+const retrieveAccessToken = () => {
+  return new Promise((resolve, reject) => {
+    const redis = require('redis')
+    const redisClient = redis.createClient(process.env.REDIS_PORT_6379_TCP_ADDR_PORT, process.env.REDIS_PORT_6379_TCP_ADDR)
+
+    redisClient.on('error', (error) => {
+      rollbar.warn(`Error connecting to Redis: ${error}`)
+      AdobeCampaign.retrieveAccessToken().then((accessToken) => {
+        disconnectClient(redisClient)
+        resolve(accessToken)
+      }).catch((error) => {
+        disconnectClient(redisClient)
+        reject(error)
+      })
+    })
+
+    redisClient.on('end', () => {
+      console.log('Disconnected from Redis')
+    })
+
+    redisClient.get(ACCESS_TOKEN, (error, response) => {
+      if (error) {
+        rollbar.warn(`Error retrieving ACCESS_TOKEN: ${error}`)
+        disconnectClient(redisClient)
+      } else if (response) {
+        disconnectClient(redisClient)
+        resolve(response)
+      }
+
+      AdobeCampaign.retrieveAccessToken().then((accessToken) => {
+        disconnectClient(redisClient)
+        resolve(accessToken)
+      }).catch((error) => {
+        disconnectClient(redisClient)
+        reject(error)
+      })
+    })
+  })
+}
+
+const disconnectClient = (redisClient) => {
+  if (redisClient.connected) {
+    redisClient.quit()
+  }
+}
+
+const asyncHandler = async (message) => {
   const placement = message.placement
   const grMasterPersonIds = message.grMasterPersonIds
 
-  const asyncHandler = async () => {
-    let accessToken = await retrieveAccessToken()
+  let accessToken = await retrieveAccessToken()
 
-    if (accessToken instanceof Error) {
-      throw new Error(accessToken)
-    }
-
-    let promises = []
-    let matchedUsers = []
-    for (let grMasterPersonId of grMasterPersonIds) {
-      const campaignUsers = await AdobeCampaign.retrieveCampaignUser(grMasterPersonId, accessToken)
-      matchedUsers = matchedUsers.concat(campaignUsers)
-    }
-
-    forEach(matchedUsers, (user) => {
-      promises.push(AdobeCampaign.updateCampaignUserPlacement(user['PKey'], placement, accessToken))
-    })
-
-    return Promise.all(promises).then((results) => {
-      return Promise.resolve(`Successfully sent placement data for ${results.length} matched users to Adobe Campaign`)
-    }).catch((error) => {
-      return Promise.reject(error)
-    })
+  let promises = []
+  let matchedUsers = []
+  for (let grMasterPersonId of grMasterPersonIds) {
+    const campaignUsers = await AdobeCampaign.retrieveCampaignUser(grMasterPersonId, accessToken)
+    matchedUsers = matchedUsers.concat(campaignUsers)
   }
 
-  const retrieveAccessToken = () => {
-    return new Promise((resolve, reject) => {
-      const redis = require('redis')
-      const redisClient = redis.createClient(process.env.REDIS_PORT_6379_TCP_ADDR_PORT, process.env.REDIS_PORT_6379_TCP_ADDR)
-
-      redisClient.on('error', (error) => {
-        rollbar.warn(`Error connecting to Redis: ${error}`)
-        AdobeCampaign.retrieveAccessToken().then((accessToken) => {
-          disconnectClient(redisClient)
-          resolve(accessToken)
-        }).catch((error) => {
-          disconnectClient(redisClient)
-          reject(error)
-        })
-      })
-
-      redisClient.on('end', () => {
-        console.log('Disconnected from Redis')
-      })
-
-      redisClient.get(ACCESS_TOKEN, (error, response) => {
-        if (error) {
-          rollbar.warn(`Error retrieving ACCESS_TOKEN: ${error}`)
-          disconnectClient(redisClient)
-        } else if (response) {
-          disconnectClient(redisClient)
-          resolve(response)
-        }
-
-        AdobeCampaign.retrieveAccessToken().then((accessToken) => {
-          disconnectClient(redisClient)
-          resolve(accessToken)
-        }).catch((error) => {
-          disconnectClient(redisClient)
-          reject(error)
-        })
-      })
-    })
-  }
-
-  const disconnectClient = (redisClient) => {
-    if (redisClient.connected) {
-      redisClient.quit()
-    }
-  }
-
-  asyncHandler().then((message) => {
-    lambdaCallback(null, message)
-  }).catch((error) => {
-    rollbar.error('Something went wrong while sending the placement to Campaign: ' + error)
-    lambdaCallback('Failed to send placement to Campaign: ' + error)
+  forEach(matchedUsers, (user) => {
+    promises.push(AdobeCampaign.updateCampaignUserPlacement(user['PKey'], placement, accessToken))
   })
-})
+
+  return Promise.all(promises).then((results) => {
+    return Promise.resolve(`Successfully sent placement data for ${results.length} matched users to Adobe Campaign`)
+  }).catch((error) => {
+    return Promise.reject(error)
+  })
+}
+
+module.exports = {
+  handler: rollbar.lambdaHandler((lambdaEvent, lambdaContext, lambdaCallback) => {
+    let snsMessage = lambdaEvent.Records[0].Sns.Message
+
+    asyncHandler(JSON.parse(snsMessage)).then((message) => {
+      lambdaCallback(null, message)
+    }).catch((error) => {
+      rollbar.error('Something went wrong while sending the placement to Campaign: ' + error)
+      lambdaCallback('Failed to send placement to Campaign: ' + error)
+    })
+  }),
+  retrieveAccessToken: retrieveAccessToken,
+  disconnectClient: disconnectClient
+}
