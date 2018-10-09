7b4d22d87b4a295ad5910ce7dbe847224cac0c03
Bill Randall <william.randall@cru.org>
2018-10-09 16:52:53 -0400

    Fix test broken by adding sns

--- __mocks__/aws-sdk.js
+++ __mocks__/aws-sdk.js
@@ -33,5 +33,15 @@ AWS.S3.prototype = {
   }
 }
 
+AWS.SNS = function () {}
+
+AWS.SNS.prototype = {
+  ...AWS.SNS.prototype,
+
+  publish (params, callback) {
+    callback(null, 1)
+  }
+}
+
 // Export my AWS function so it can be referenced via requires
 module.exports = AWS
