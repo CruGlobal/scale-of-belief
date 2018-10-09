4e68e8840c3aca734f864ca84753dc643fe936b0
Bill Randall <william.randall@cru.org>
2018-10-09 09:24:17 -0400

    Send sns message to sync scores to AEM when a POST to /score happens

--- api/controllers/score.js
+++ api/controllers/score.js
@@ -4,6 +4,7 @@ const Score = require('../../models/score')
 const RecentlyScored = require('../../models/recently-scored')
 const util = require('../util/util')
 const {pick} = require('lodash')
+const AWS = require('aws-sdk')
 
 const get = (request, response) => {
   var uri = util.sanitizeUri(request.query['uri'])
@@ -25,6 +26,7 @@ const post = (request, response) => {
 
   Score.save(sanitizedUri, pick(requestBody, ['score', 'weight'])).then(async (result) => {
     await RecentlyScored.save(sanitizedUri, requestBody.score)
+    await sendSns(sanitizedUri, requestBody.score)
 
     // On update, we will have a multi-dimensional array (first element being the version), on create we won't
     if (Array.isArray(result)) {
@@ -32,6 +34,27 @@ const post = (request, response) => {
     } else {
       response.json(Score.toApiScore(result.dataValues))
     }
+  })
+}
+
+const sendSns = async (sanitizedUri, score) => {
+  const sns = new AWS.SNS({ region: 'us-east-1' })
+  const payload = {
+    uri: sanitizedUri,
+    score: score
+  }
+  const params = {
+    Message: JSON.stringify(payload),
+    TopicArn: process.env.AEM_SNS_TOPIC_ARN
+  }
+
+  return new Promise((resolve, reject) => {
+    sns.publish(params, (error, data) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(data)
+    })
   })
 }
 
