9bd1a099148fa864033214ef989331d0d22b5068
Brian Zoetewey <brian.zoetewey@ccci.org>
2019-06-03 16:13:59 -0400

    Add LOG_LEVEL environment param

--- serverless/environment.js
+++ serverless/environment.js
@@ -11,6 +11,7 @@ module.exports = () => {
     DB_PORT_5432_TCP_ADDR: process.env['DB_PORT_5432_TCP_ADDR'] || 'localhost',
     DB_PORT_5432_TCP_PORT: process.env['DB_PORT_5432_TCP_PORT'] || 5432,
     ROLLBAR_ACCESS_TOKEN: process.env['ROLLBAR_ACCESS_TOKEN'] || '',
+    LOG_LEVEL: process.env['LOG_LEVEL'] || 'info',
     JWT_SECRET: process.env['JWT_SECRET'] || 'secret',
     THE_KEY_SERVICE_URL: process.env['THE_KEY_SERVICE_URL'] || 'http://localhost:3000/api/login',
     THE_KEY_API_KEY: process.env['THE_KEY_API_KEY'],
