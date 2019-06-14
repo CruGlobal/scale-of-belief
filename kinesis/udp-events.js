9f212bb141a8e53a36ba3b66f3ad7b87d590e593
Brian Zoetewey <brian.zoetewey@cru.org>
2019-06-14 14:57:45 -0400

    Add udp-events kinesis consumer to test sending SQS events.

---
+++ kinesis/udp-events.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const rollbar = require('../config/rollbar')
+const AWS = require('aws-sdk')
+
+module.exports.handler = rollbar.lambdaHandler((lambdaEvent, lambdaContext, lambdaCallback) => {
+  const Event = require('../models/event')
+
+  // Make sure we have event records
+  if (typeof lambdaEvent['Records'] !== 'undefined') {
+    const validEvents = []
+    // Iterate over each record
+    lambdaEvent['Records'].forEach((record) => {
+      try {
+        // Build an event object from each record, catch any resulting errors (InvalidEventError)
+        validEvents.push(Event.fromRecord(record))
+      } catch (error) {
+        rollbar.error('Event.fromRecord(record) error', error, {record: record})
+      }
+    })
+
+    if (validEvents.length > 0) {
+      const sqs = new AWS.SQS({apiVersion: '2012-11-05', region: 'us-east-1'})
+      const entries = validEvents.map((event => ({
+        Id: event.event_id,
+        MessageBody: JSON.stringify(event.toJSON())
+      })))
+
+      sqs.sendMessageBatch({
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/056154071827/scale-of-belief-production-UDPEnrichedEventsQueue-1C100YLPZXPHF',
+        Entries: entries
+      }, (err, data) => {
+        lambdaCallback(null, data)
+      })
+    } else {
+      lambdaCallback(null, 'Nothing processed')
+    }
+  } else {
+    lambdaCallback(null, 'Nothing processed')
+  }
+})
