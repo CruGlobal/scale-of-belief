e9b3db73763b5a1b82c71fd9c188a32d1252cdc6
Brian Zoetewey <brian.zoetewey@cru.org>
2019-06-18 10:14:09 -0400

    Include LOG_LEVEL in lambda environment vars

--- serverless/environment.js
+++ serverless/environment.js
@@ -34,6 +34,7 @@ module.exports = () => {
     AEM_URL: process.env['AEM_URL'] || 'http://localhost:4502',
     AEM_USERNAME: process.env['AEM_USERNAME'] || '',
     AEM_PASSWORD: process.env['AEM_PASSWORD'] || '',
-    AEM_SNS_TOPIC_ARN: process.env['AEM_SNS_TOPIC_ARN'] || ''
+    AEM_SNS_TOPIC_ARN: process.env['AEM_SNS_TOPIC_ARN'] || '',
+    LOG_LEVEL: process.env['LOG_LEVEL'] || 'error'
   }
 }
