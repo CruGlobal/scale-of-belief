8ac1dc6ddc7bef6f0e29f829245de4d68cdbf76e
cru ds <cruds@crus-MacBook-Pro.local>
2019-09-23 18:15:50 +0800

    add new uris.js file

---
+++ api/controllers/uris.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const {forEach} = require('lodash')
+const util = require('../util/util')
+const Unscored = require('../../models/unscored')
+const Score = require('../../models/score')
+const {Op} = require('sequelize')
+
+const get = (frequest, response) => {
+  let page = request.query['page']
+  let perPage = request.query['per_page']
+  let order = request.query['order']
+
+  if (!util.isInt(page) || !util.isInt(perPage)) {
+    page = 1
+    perPage = 25
+  }
+
+  if (!order) {
+    order = 'ASC'
+  }
+
+  const offset = (page - 1) * perPage
+
+  let urisList = []
+  Unscored.findAndCountAll({
+    limit: perPage,
+    offset: offset,
+    order: [['uri', order]]
+  }).then((results) => {
+    forEach(results.rows, (result) => {
+      urisList.push(Score.toUnscored(result.uri))
+    })
+  })
+
+  Score.findAndCountAll({
+    limit: perPage,
+    offset: offset,
+    order: [[orderBy, order]]
+  }).then((scores) => {
+    forEach(scores.rows, (score) => {
+      urisList.push(Score.toUriScore(score))
+    })
+    response.json({ data: urisList, meta: { total: scores.count } })
+  })
+}
+
+module.exports = { get: get }
