b197af8785c91c76dd0b642be1712060d05e20bf
jonahktjala <jonahktjala>
2019-11-07 21:53:28 +0800

    added handler for unscored URIs and s3 upload

--- scheduled/uri-scored.js
+++ scheduled/uri-scored.js
@@ -2,7 +2,9 @@
 
 const AWS = require('aws-sdk');
 const Score = require('../models/score')
+const Unscored = require('../models/unscored')
 var awsConfig = require('aws-config');
+const ObjectsToCsv = require('objects-to-csv');
 
 //load configuration file from path
 const awsConfigJson = require('../config/s3-config.json');
@@ -10,17 +12,34 @@ const s3bucket = new AWS.S3(awsConfig(awsConfigJson));
 
 /**
  * a function to get all URI scored and unscored from the database
+ * returns a -1 for unscored uris
  */
 module.exports.handler = async (lambdaEvent) => {
   const path = lambdaEvent.queryStringParameters.path;
   const bucketName = lambdaEvent.queryStringParameters.bucketName;
+  var unscored = [];
+  var scored = [];
+  var allUris = [];
+
+  await Unscored.getAllUris().then((response) => {
+    unscored = response;
+  })
   await Score.getAllScores().then( (response) => {
-    
-    var data = Buffer.from(response);
+    scored = response;
+    allUris = scored.concat(unscored);
+  }) 
+
+  await new ObjectsToCsv(allUris).toDisk(path).then((response) => {
+    // var data = Buffer.from(allUris);
+    // var csv = printCsv(allUris);
+    var csv = response;
+    console.log(csv);
+    console.log(typeof(csv));
+    // var data = Buffer.from(csv);
     var param = {
       Bucket: bucketName,
       Key: path,
-      Body: data
+      Body: csv
     };
 
     s3bucket.putObject(param, function(err, response) {
@@ -29,7 +48,6 @@ module.exports.handler = async (lambdaEvent) => {
       } else {
         console.log(response)
       }
-    });
-
-  })  
+    });  
+  }) 
 }
