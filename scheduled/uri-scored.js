f602c3493bef61ac7a67b9a78089ccd608d91a39
jonahktjala <jonahktjala>
2019-11-13 22:47:43 +0800

    removed extra ;

--- scheduled/uri-scored.js
+++ scheduled/uri-scored.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 const rollbar = require('../config/rollbar')
 const Score = require('../models/score')
 const Unscored = require('../models/unscored')
-const ObjectsToCsv = require('objects-to-csv');
+const ObjectsToCsv = require('objects-to-csv')
 
 /**
  * a function to get all URI scored and unscored from the database
@@ -12,38 +12,38 @@ const ObjectsToCsv = require('objects-to-csv');
  * author: jonahkjala
  */
 module.exports.handler = async (lambdaEvent) => {
-  const path = "scores.csv";
-  const bucketName = process.env.S3_SCORED_URIS_BUCKET;
+  const path = "scores.csv"
+  const bucketName = process.env.S3_SCORED_URIS_BUCKET
   const s3bucket = new AWS.S3({apiVersion: '2006-03-01'})
   
-  var unscored = [];
-  var scored = [];
-  var allUris = [];
+  var unscored = []
+  var scored = []
+  var allUris = []
 
   await Unscored.getAllUris().then((response) => {
-    unscored = response;
+    unscored = response
   })
   await Score.getAllScores().then( (response) => {
-    scored = response;
-    allUris = scored.concat(unscored);
+    scored = response
+    allUris = scored.concat(unscored)
   }) 
 
   await new ObjectsToCsv(allUris).toString().then((response) => {
-    var csv = response;
+    var csv = response
     var param = {
       Bucket: bucketName,
       Key: path,
       Body: csv
-    };
+    }
 
     s3bucket.putObject(param, function(err, response) {
       if(err) {
         rollbar.error('Upload csv error: ' + err, err)
         throw err
       } else {
-        rollbar.warn(response);
+        rollbar.warn(response)
         return response
       }
-    });  
+    })
   }) 
 }
