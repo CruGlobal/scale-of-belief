cc8bbf64638e4b3ece60ef9e34729e8a34f57544
jonahktjala <jonahktjala>
2019-11-13 23:58:23 +0800

    modified copy to s3

--- scheduled/uri-scored.test.js
+++ scheduled/uri-scored.test.js
@@ -2,21 +2,22 @@
 
 const lambda = require('./uri-scored')
 const requestMock = require('request-promise-native')
+const {Client} = require('pg')
+
+jest.mock('pg', () => ({
+    Client: jest.fn()
+  }))
 
 describe('uri-scored', () => {
   it('Should be defined', () => {
     expect(lambda).toBeDefined()
   })
 
-  describe('created csv in s3', () => {
+  describe('success in copy to s3', () => {
     it('should succeed', async () => {
       requestMock.mockResolvedValueOnce({hits: []})
       expect.assertions(1)
-      await expect(lambda.handler()).toHaveProperty('ETag')
+      await expect(lambda.handler()).resolves.not.toBeNull()
     })
-  })
-  
-  test('the fetch fails with an error', async () => {
-    await expect(lambda.handler()).rejects.toThrow('error')
   })
 })
