6eebfc8aa1a74296459cc4b2c4e2e54d2a53e7ab
jonahktjala <jonahktjala>
2020-02-05 23:27:33 +0800

    improved testing for getting unscored uris

--- models/unscored.test.js
+++ models/unscored.test.js
@@ -1,8 +1,6 @@
 'use strict'
 
 const Unscored = require('./unscored')
-const factory = require('../test/factory')
-const { omit } = require('lodash')
 const paperTrail = require('../config/papertrail')
 const Revisions = paperTrail['revisions']
 
@@ -13,47 +11,35 @@ describe('Unscored', () => {
 
   // created by: jonahktjala
   describe('Unscored.toScoreObject()', () => {
-    let unscoredUri
-    beforeEach(() => {
-      return factory.create('existing_unscored').then(existingUnscored => { unscoredUri = existingUnscored })
-    })
-
-    test('should return formatted object', () => {
+    it('should create a json object', () => {
+      const unscoredUri = {
+        uri: 'http://someuri.com',
+        last_refreshed: new Date()
+      }
       const expectedApiScore = {
         uri: unscoredUri.uri,
         score: -1,
         weight: 0
       }
-      expect.assertions(3)
-      expect(unscoredUri).toBeDefined()
-      return Unscored.toScoreObject(unscoredUri).then((result) => {
-        const exclude = ['last_refreshed']
-        expect(result).toBeDefined()
-        expect(result).not.toBeNull()
-        expect(omit(result, exclude)).toEqual(expectedApiScore)
-      })
+      const unscored = Unscored.toScoreObject(unscoredUri)
+      expect(unscored).toBeDefined()
+      expect(unscored).toEqual(expectedApiScore)
     })
   })
 
   // created by: jonahktjala
-  describe('Unscored.getAllUris()', () => {
-    let score
-    beforeEach(() => {
-      return factory.create('existing_score').then(existingScore => { score = existingScore })
-    })
-
-    it('should not return a null array of scored objects', () => {
-      return Unscored.getAllUris().then((result) => {
+  describe('Unscored.getAllUris() matching https', () => {
+    const expected = [
+      expect.stringMatching('^(http|https)://')
+    ]
+    test('Unscored.getAllUris valid and not null', () => {
+      return Unscored.getAllUris().then(result => {
         // make sure that output is not null
-        expect(result).not.toBeNull()
         expect(result).toBeDefined()
-        let oneEntry = result[0]
-
-        // check that property values are not null
-        const exclude = ['revision']
-        expect(oneEntry).toBeDefined()
-        expect(oneEntry).not.toBeNull()
-        expect(oneEntry).toHaveProperty(omit(score.dataValues, exclude).keys())
+        // make sure it contains https
+        expect(result).toEqual(
+          expect.arrayContaining(expected)
+        )
       })
     })
   })
