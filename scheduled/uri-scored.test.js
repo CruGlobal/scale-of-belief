9e122b0ea9bf8ae1a9ba8280f692dcf8ae31f700
jonahktjala <jonahktjala>
2020-02-12 22:44:57 +0800

    add jest for putObject

--- scheduled/uri-scored.test.js
+++ scheduled/uri-scored.test.js
@@ -15,5 +15,5 @@ test('test for putObject in s3', async () => {
       Body: 'success'
     }
   })
-  expect(await lambda.handler()).toEqual('success')
+  expect(jest.mock.putObject).toHaveBeenCalledWith(params)
 })
